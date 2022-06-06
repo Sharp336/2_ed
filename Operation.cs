419de140d79591b997ae7c0db08941c03d4d4d42
Sharp336 <39415926+Sharp336@users.noreply.github.com>
2022-06-06 15:50:15 +0300

    Add files via upload

---
+++ Operation.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics.SymbolStore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace opera;
+
+public class Operation
+{
+    public string Trigger;
+    public Func<List<string>, List<string>> Fn;
+    public static List<Operation> List = new();
+
+    public Operation(string trigger, Func<List<string>, List<string>> fn)
+    {
+        Trigger = trigger;
+        Fn = fn;
+        List.Add(this);
+    }
+
+    public static void CreateOperations()
+    {
+
+        List<string> ToLower(List<string> var)
+        {
+            for (int i = 0; i < var.Count; i++)
+            {
+                var[i] = var[i].ToLower();
+            }
+            return var;
+        }
+        
+        List<string> ToUpper(List<string> var)
+        {
+            for (int i = 0; i < var.Count; i++)
+            {
+                var[i] = var[i].ToUpper();
+            }
+            return var;
+        }
+
+        List<string> Replace(List<string> var)
+        {
+            Console.WriteLine("Replace > ");
+            string oldValue = Console.ReadLine();
+            Console.WriteLine("With > ");
+            string newValue = Console.ReadLine();
+            for (int i = 0; i < var.Count; i++)
+            {
+                var[i] = var[i].Replace(oldValue, newValue);
+            }
+
+            return var;
+        }
+
+        new Operation("tolower", ToLower);
+        new Operation("toupper", ToUpper);
+        new Operation("replace", Replace);
+    }
+}
